182b0dd88d28ed0f1496d1b95d948f5639776203
Moayad Abukhadra <moayadabukhadra54@gmail.com>
2023-03-30 15:32:48 +0300

    Update UserController.php

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -70,14 +70,14 @@ class UserController extends Controller
      */
     public function userRole()
     {
-        $user = Auth::user();
+        $user = \auth('api')->user();
         return response()->json(['success' => $user->roles()->first()->name], $this->successStatus);
 
     }
 
     function favoriteBooks(Request $request)
     {
-        $user = Auth::user();
+        $user = \auth('api')->user();
         $paginate = $request->get('paginate') ?? 10;
         return response()->json(['success' => $user->favoriteBooks()->with(['image','author','category'])->paginate($paginate)], $this->successStatus);
     }
