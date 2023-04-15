36f2eda0c34f3720c5993fd1c1ca69613c5c067d
Moayad Abukhadra <moayadabukhadra54@gmail.com>
2023-04-15 22:02:18 +0300

    Update BookMarkController.php

--- app/Http/Controllers/api/BookMarkController.php
+++ app/Http/Controllers/api/BookMarkController.php
@@ -22,7 +22,7 @@ class BookMarkController extends Controller
             return response()->json(['error' => 'يجب تسجيل الدخول اولا'], 401);
         }
 
-        return response()->json(['success' => $user->bookmarks()->with(['book', 'bookmark_types'])->groupBy('bookmark_types.name')], 201);
+        return response()->json(['success' => $user->bookmarks()->with(['book', 'bookmark_types'])->groupBy('bookmark_types.name')->get()], 201);
     }
 
     public function favoriteBooks()
