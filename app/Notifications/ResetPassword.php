17f330a55603bc3238901c93220e119c6ddf562f
Moayad Abukhadra <moayadabukhadra54@gmail.com>
2023-04-25 21:17:42 +0300

    Update ResetPassword.php

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -37,7 +37,7 @@ class ResetPassword extends Notification
     {
         return (new MailMessage)
             ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', "http://localhost:3000" . '/reset-password/' . $this->token)
+            ->action('Reset Password', "http://localhost:3000" . '/auth/reset-password/' . $this->token)
             ->line('If you did not request a password reset, no further action is required.');
     }
 
