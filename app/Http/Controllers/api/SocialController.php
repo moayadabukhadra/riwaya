78de659dbaf36c78e7d17c4379d2b7ca2da65b14
Moayad Abukhadra <moayadabukhadra54@gmail.com>
2023-05-07 15:31:04 +0300

    Update SocialController.php

--- app/Http/Controllers/api/SocialController.php
+++ app/Http/Controllers/api/SocialController.php
@@ -13,12 +13,17 @@ class SocialController extends Controller
 {
     public function facebookRedirect()
     {
+        header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 
         return Socialite::driver('facebook')->redirect();
     }
 
     public function loginWithFacebook()
     {
+        header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+
         return Socialite::driver('facebook')->redirect();    }
 
 }
